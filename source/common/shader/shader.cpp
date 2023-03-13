1c24fe19304a20bc47899d59d712b61ada708a3c
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2023-03-13 20:15:59 +0200

    implement attach method in shader class

--- source/common/shader/shader.cpp
+++ source/common/shader/shader.cpp
@@ -7,29 +7,50 @@
 
 //Forward definition for error checking functions
 std::string checkForShaderCompilationErrors(GLuint shader);
+
 std::string checkForLinkingErrors(GLuint program);
 
 bool our::ShaderProgram::attach(const std::string &filename, GLenum type) const {
     // Here, we open the file and read a string from it containing the GLSL code of our shader
     std::ifstream file(filename);
-    if(!file){
+    if (!file) {
         std::cerr << "ERROR: Couldn't open shader file: " << filename << std::endl;
         return false;
     }
     std::string sourceString = std::string(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>());
-    const char* sourceCStr = sourceString.c_str();
+    const char *sourceCStr = sourceString.c_str();
     file.close();
 
-    //TODO: Complete this function
+    //DONE: Complete this function
     //Note: The function "checkForShaderCompilationErrors" checks if there is
     // an error in the given shader. You should use it to check if there is a
     // compilation error and print it so that you can know what is wrong with
     // the shader. The returned string will be empty if there is no errors.
 
+    // Create shader with received type (GL_VERTEX_SHADER or GL_FRAGMENT_SHADER)
+    uint8_t shaderID = glCreateShader(type);
+
+    // Send shader source code
+    glShaderSource(shaderID, 1, &sourceCStr, NULL);
+
+    // Compile the shader
+    glCompileShader(shaderID);
+
+    // Check for errors in shader
+    std::string error = checkForShaderCompilationErrors(shaderID);
+
+    // If there is an error message
+    if (!error.empty()) {
+        // Print the error message
+        std::cerr << "ERROR::SHADER::" << (type == GL_VERTEX_SHADER ? "VERTEX" : "FRAGMENT") << "::COMPILATION_FAILED\n"
+                  << error << std::endl;
+
+        // We return false if the compilation failed
+        return false;
+    }
     //We return true if the compilation succeeded
     return true;
 }
-
 
 
 bool our::ShaderProgram::link() const {
@@ -46,8 +67,8 @@ bool our::ShaderProgram::link() const {
 // Function to check for compilation and linking error in shaders //
 ////////////////////////////////////////////////////////////////////
 
-std::string checkForShaderCompilationErrors(GLuint shader){
-     //Check and return any error in the compilation process
+std::string checkForShaderCompilationErrors(GLuint shader) {
+    //Check and return any error in the compilation process
     GLint status;
     glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
     if (!status) {
@@ -62,8 +83,8 @@ std::string checkForShaderCompilationErrors(GLuint shader){
     return std::string();
 }
 
-std::string checkForLinkingErrors(GLuint program){
-     //Check and return any error in the linking process
+std::string checkForLinkingErrors(GLuint program) {
+    //Check and return any error in the linking process
     GLint status;
     glGetProgramiv(program, GL_LINK_STATUS, &status);
     if (!status) {
