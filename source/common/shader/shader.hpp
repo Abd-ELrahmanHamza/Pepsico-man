7de30b8869589a10fcc645c7a6a025bb5bc301bf
Ahmedsabry11 <ahmedsabry232345@gmail.com>
2023-03-13 20:14:00 +0200

    finish shader.hpp requirements

--- source/common/shader/shader.hpp
+++ source/common/shader/shader.hpp
@@ -7,64 +7,96 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-namespace our {
+namespace our
+{
 
-    class ShaderProgram {
+    class ShaderProgram
+    {
 
     private:
-        //Shader Program Handle (OpenGL object name)
+        // Shader Program Handle (OpenGL object name)
         GLuint program;
 
     public:
-        ShaderProgram(){
-            //TODO: (Req 1) Create A shader program
+        ShaderProgram()
+        {
+            // TODO: (Req 1) Create A shader program
+            program = glCreateProgram();
         }
-        ~ShaderProgram(){
-            //TODO: (Req 1) Delete a shader program
+        ~ShaderProgram()
+        {
+            // TODO: (Req 1) Delete a shader program
+            glDeleteProgram(program);
         }
 
         bool attach(const std::string &filename, GLenum type) const;
 
         bool link() const;
 
-        void use() { 
+        void use()
+        {
             glUseProgram(program);
         }
 
-        GLuint getUniformLocation(const std::string &name) {
-            //TODO: (Req 1) Return the location of the uniform with the given name
+        GLuint getUniformLocation(const std::string &name)
+        {
+            // TODO: (Req 1) Return the location of the uniform with the given name
+            return glGetUniformLocation(program, name.c_str());
         }
 
-        void set(const std::string &uniform, GLfloat value) {
-            //TODO: (Req 1) Send the given float value to the given uniform
+        void set(const std::string &uniform, GLfloat value)
+        {
+            // TODO: (Req 1) Send the given float value to the given uniform
+            GLuint uniformLocation = getUniformLocation(uniform);
+            glUniform1f(uniformLocation, value);
         }
 
-        void set(const std::string &uniform, GLuint value) {
-            //TODO: (Req 1) Send the given unsigned integer value to the given uniform
+        void set(const std::string &uniform, GLuint value)
+        {
+            // TODO: (Req 1) Send the given unsigned integer value to the given uniform
+            GLuint uniformLocation = getUniformLocation(uniform);
+            glUniform1ui(uniformLocation, value);
         }
 
-        void set(const std::string &uniform, GLint value) {
-            //TODO: (Req 1) Send the given integer value to the given uniform
+        void set(const std::string &uniform, GLint value)
+        {
+            // TODO: (Req 1) Send the given integer value to the given uniform
+            GLuint uniformLocation = getUniformLocation(uniform);
+            glUniform1i(uniformLocation, value);
         }
 
-        void set(const std::string &uniform, glm::vec2 value) {
-            //TODO: (Req 1) Send the given 2D vector value to the given uniform
+        void set(const std::string &uniform, glm::vec2 value)
+        {
+            // TODO: (Req 1) Send the given 2D vector value to the given uniform
+            GLuint uniformLocation = getUniformLocation(uniform);
+            glUniform2fv(uniformLocation, 2, &value[0]);
         }
 
-        void set(const std::string &uniform, glm::vec3 value) {
-            //TODO: (Req 1) Send the given 3D vector value to the given uniform
+        void set(const std::string &uniform, glm::vec3 value)
+        {
+            // TODO: (Req 1) Send the given 3D vector value to the given uniform
+            GLuint uniformLocation = getUniformLocation(uniform);
+            glUniform3fv(uniformLocation, 2, &value[0]);
         }
 
-        void set(const std::string &uniform, glm::vec4 value) {
-            //TODO: (Req 1) Send the given 4D vector value to the given uniform
+        void set(const std::string &uniform, glm::vec4 value)
+        {
+            // TODO: (Req 1) Send the given 4D vector value to the given uniform
+            GLuint uniformLocation = getUniformLocation(uniform);
+            glUniform4fv(uniformLocation, 2, &value[0]);
         }
 
-        void set(const std::string &uniform, glm::mat4 matrix) {
-            //TODO: (Req 1) Send the given matrix 4x4 value to the given uniform
+        void set(const std::string &uniform, glm::mat4 matrix)
+        {
+            // TODO: (Req 1) Send the given matrix 4x4 value to the given uniform
+            GLuint uniformLocation = getUniformLocation(uniform);
+            glUniformMatrix4fv(uniformLocation, 1, false, &matrix[0][0]);
         }
 
-        //TODO: (Req 1) Delete the copy constructor and assignment operator.
-        //Question: Why do we delete the copy constructor and assignment operator?
+        // TODO: (Req 1) Delete the copy constructor and assignment operator.
+        // Question: Why do we delete the copy constructor and assignment operator?
+        ShaderProgram(const ShaderProgram &) = delete;
+        ShaderProgram &operator=(const ShaderProgram &) = delete;
     };
 
 }
