fccfff2cd40c7fa666454c0734c12d93a1171b4b
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2023-04-28 20:01:23 +0300

    document requirement 5

--- source/common/texture/texture-utils.cpp
+++ source/common/texture/texture-utils.cpp
@@ -6,8 +6,7 @@
 
 #include <iostream>
 
-our::Texture2D *our::texture_utils::empty(GLenum format, glm::ivec2 size)
-{
+our::Texture2D *our::texture_utils::empty(GLenum format, glm::ivec2 size) {
     // our::Texture2D *texture = new our::Texture2D();
     // // TODO: (Req 11) Finish this function to create an empty texture with the given size and format
     // texture->bind();
@@ -36,8 +35,7 @@ our::Texture2D *our::texture_utils::empty(GLenum format, glm::ivec2 size)
     return texture;
 }
 
-our::Texture2D *our::texture_utils::loadImage(const std::string &filename, bool generate_mipmap)
-{
+our::Texture2D *our::texture_utils::loadImage(const std::string &filename, bool generate_mipmap) {
     glm::ivec2 size;
     int channels;
     // Since OpenGL puts the texture origin at the bottom left while images typically has the origin at the top left,
@@ -52,8 +50,7 @@ our::Texture2D *our::texture_utils::loadImage(const std::string &filename, bool 
     //- 4: RGB and Alpha (RGBA)
     // Note: channels (the 4th argument) always returns the original number of channels in the file
     unsigned char *pixels = stbi_load(filename.c_str(), &size.x, &size.y, &channels, 4);
-    if (pixels == nullptr)
-    {
+    if (pixels == nullptr) {
         std::cerr << "Failed to load image: " << filename << std::endl;
         return nullptr;
     }
@@ -62,10 +59,26 @@ our::Texture2D *our::texture_utils::loadImage(const std::string &filename, bool 
     // Bind the texture such that we upload the image data to its storage
     // TODO: (Req 5) Finish this function to fill the texture with the data found in "pixels"
 
+    // Bind the texture object
     texture->bind();
+    // create a new two-dimensional texture object in memory and initialize it with data.
+    // Type of texture: GL_TEXTURE_2D
+    // Level of detail: 0
+    // Internal format: GL_RGBA8
+    // Width: size.x
+    // Height: size.y
+    // Border: 0
+    // Format: GL_RGBA
+    // Type: GL_UNSIGNED_BYTE
+    // Data: pixels (the image data)
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, size.x, size.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+
+    // if generate_mipmap variable is true, generate mipmaps for the texture
+    // Mipmaps are pre-calculated chains of optimized textures. Each texture in the chain is a progressively lower resolution representation of the same image.
     if (generate_mipmap)
         glGenerateMipmap(GL_TEXTURE_2D);
+
+    // The purpose of this function is to free the memory that was allocated by the stbi_load function, which is used to load an image into memory.
     stbi_image_free(pixels); // Free image data after uploading to GPU
     return texture;
 }
--- source/common/texture/texture2d.hpp
+++ source/common/texture/texture2d.hpp
@@ -12,12 +12,14 @@ namespace our {
         // This constructor creates an OpenGL texture and saves its object name in the member variable "name" 
         Texture2D() {
             //TODO: (Req 5) Complete this function
+            // generate texture using name of texture
             glGenTextures(1, &this->name);
         };
 
         // This deconstructor deletes the underlying OpenGL texture
         ~Texture2D() {
             //TODO: (Req 5) Complete this function
+            // delete texture using name of texture
             glDeleteTextures(1, &this->name);
         }
 
@@ -29,12 +31,14 @@ namespace our {
         // This method binds this texture to GL_TEXTURE_2D
         void bind() const {
             //TODO: (Req 5) Complete this function
+            // bind texture to GL_TEXTURE_2D using name of texture
             glBindTexture(GL_TEXTURE_2D, this->name);
         }
 
         // This static method ensures that no texture is bound to GL_TEXTURE_2D
         static void unbind() {
             //TODO: (Req 5) Complete this function
+            // unbind texture from GL_TEXTURE_2D by passing 0 to GL_TEXTURE_2D
             glBindTexture(GL_TEXTURE_2D, 0);
         }
 
