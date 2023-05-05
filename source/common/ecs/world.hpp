cf5a648d1ca0406cb5d8c19f081d7a13345fd7ab
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2023-05-05 18:55:41 +0300

    feat: add obstacles

--- source/common/ecs/world.hpp
+++ source/common/ecs/world.hpp
@@ -3,16 +3,14 @@
 #include <unordered_set>
 #include "entity.hpp"
 
-namespace our
-{
+namespace our {
 
     // This class holds a set of entities
-    class World
-    {    public:
+    class World {
         std::unordered_set<Entity *> entities;         // These are the entities held by this world
         std::unordered_set<Entity *> markedForRemoval; // These are the entities that are awaiting to be deleted
         // when deleteMarkedEntities is called
-
+    public:
         World() = default;
 
         // This will deserialize a json array of entities and add the new entities to the current world
@@ -24,8 +22,7 @@ namespace our
         // WARNING The entity is owned by this world so don't use "delete" to delete it, instead, call "markForRemoval"
         // to put it in the "markedForRemoval" set. The elements in the "markedForRemoval" set will be removed and
         // deleted when "deleteMarkedEntities" is called.
-        Entity *add()
-        {
+        Entity *add() {
             //(Req 8) Create a new entity, set its world member variable to this,
             // and don't forget to insert it in the suitable container.
             Entity *newEntity = new Entity(); // create a new entity
@@ -35,30 +32,27 @@ namespace our
         }
 
         // This returns and immutable reference to the set of all entites in the world.
-        const std::unordered_set<Entity *> &getEntities()
-        {
+        const std::unordered_set<Entity *> &getEntities() {
             return entities;
         }
 
         // This marks an entity for removal by adding it to the "markedForRemoval" set.
         // The elements in the "markedForRemoval" set will be removed and deleted when "deleteMarkedEntities" is called.
-        void markForRemoval(Entity *entity)
-        {
+        void markForRemoval(Entity *entity) {
             //(Req 8) If the entity is in this world, add it to the "markedForRemoval" set.
             if (entities.find(entity) != entities.end()) // check if the entity is in this world
-                this->markedForRemoval.insert(entity);   // add the entity to the markedForRemoval set to be deleted later
+                this->markedForRemoval.insert(
+                        entity);   // add the entity to the markedForRemoval set to be deleted later
         }
 
         // This removes the elements in "markedForRemoval" from the "entities" set.
         // Then each of these elements are deleted.
-        void deleteMarkedEntities()
-        {
+        void deleteMarkedEntities() {
             //(Req 8) Remove and delete all the entities that have been marked for removal
-//             for (auto it = markedForRemoval.begin(); it != markedForRemoval.end(); ++it) {
-//                 delete *it;
-//             }
-            for (auto entity : markedForRemoval)
-            {                           // loop over the markedForRemoval set
+            // for (auto it = markedForRemoval.begin(); it != markedForRemoval.end(); ++it) {
+            //     delete *it;
+            // }
+            for (auto entity: markedForRemoval) {                           // loop over the markedForRemoval set
                 entities.erase(entity); // remove the entity from the entities set
                 delete entity;          // delete the entity
             }
@@ -66,8 +60,7 @@ namespace our
         }
 
         // This deletes all entities in the world
-        void clear()
-        {
+        void clear() {
             //(Req 8) Delete all the entites and make sure that the containers are empty
             // solution 1
 
@@ -78,8 +71,8 @@ namespace our
 
             // solution 2
 
-            for (auto it = entities.begin(); it != entities.end(); ++it)
-            {                        // loop over the entities set
+            for (auto it = entities.begin();
+                 it != entities.end(); ++it) {                        // loop over the entities set
                 markForRemoval(*it); // mark the entity for removal
             }
             deleteMarkedEntities(); // delete the marked entities
@@ -87,8 +80,7 @@ namespace our
         }
 
         // Since the world owns all of its entities, they should be deleted alongside it.
-        ~World()
-        {
+        ~World() {
             clear();
         }
 
