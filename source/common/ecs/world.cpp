34ac33218a07efa889ab9514dce49312cc660577
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2023-05-08 00:12:01 +0300

    feat: random placement logic

--- source/common/ecs/world.cpp
+++ source/common/ecs/world.cpp
@@ -1,7 +1,24 @@
 #include "world.hpp"
 #include "../deserialize-utils.hpp"
+#include <vector>
+#include <iostream>
 
 namespace our {
+    // This is the upper limit on the number of entities in the world
+    const uint64_t ENTITIES_UPPER_LIMIT = 300;
+    const uint8_t SLICE_SIZE = 9.0;
+
+    // This is the map that keeps track of entities positions in the world
+    // We have three ways left, middle and right
+    std::vector<std::vector<bool>> entityMap(ENTITIES_UPPER_LIMIT, std::vector<bool>(3, false));
+
+    int generateRandomNumber(int min, int max) {
+
+        // Generate a random number between min and max (inclusive)
+        int randomNumber = rand() % (max - min + 1) + min;
+
+        return randomNumber;
+    }
 
     // This will deserialize a json array of entities and add the new entities to the current world
     // If parent pointer is not null, the new entities will be have their parent set to that given pointer
@@ -19,26 +36,26 @@ namespace our {
                 //(Req 8) Recursively call this world's "deserialize" using the children data
                 //  and the current entity as the parent
                 this->deserialize(entityData["children"], newEntity); // deserialize the children of the current entity
-                // if (const auto &children = entityData["children"]; children.is_array())
-                // {
-                //     for (auto &child : children)
-                //     {
-                //         this->deserialize(child, newEntity);
-                //     }
-                // }
-                // else
-                // {
-                //     this->deserialize(entityData["children"], newEntity);
-                // }
             }
             if (entityData.contains("duplicates")) {
-                glm::vec2 duplicates = glm::vec2(entityData.value("duplicates", duplicates));
-
+                glm::vec3 duplicates = glm::vec3(entityData.value("duplicates", duplicates));
                 for (int i = 1; i < (int) duplicates[0]; ++i) {
                     Entity *newEntity = add();           // create a new entity using the add function in world.hpp
                     newEntity->parent = parent;          // set the parent of the new entity to the given parent
                     newEntity->deserialize(entityData);  // deserialize the new entity using the given entityData
-                    newEntity->localTransform.position.x += -i * duplicates[1];
+                    if ((bool) duplicates[2]) {
+                        int horizontal = generateRandomNumber(0, 2);
+                        int vertical = generateRandomNumber(0, ENTITIES_UPPER_LIMIT - 1);
+                        while (entityMap[vertical][horizontal]) {
+                            horizontal = generateRandomNumber(0, 2);
+                            vertical = generateRandomNumber(0, ENTITIES_UPPER_LIMIT - 1);
+                        }
+                        newEntity->localTransform.position.x += (-float(vertical)) * SLICE_SIZE;
+                        newEntity->localTransform.position.z = (-5.0f + float(horizontal) * 5.0f);
+                    } else {
+                        newEntity->localTransform.position.x += -float(i) * duplicates[1];
+                    }
+
                 }
 
             }
