c42cf579eb36437f1865458718b09db60a854dad
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2023-05-14 23:37:12 +0300

    feat: add winning state

--- source/main.cpp
+++ source/main.cpp
@@ -17,9 +17,9 @@
 #include "states/material-test-state.hpp"
 #include "states/entity-test-state.hpp"
 #include "states/renderer-test-state.hpp"
+#include "states/winning-state.hpp"
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
 
     flags::args args(argc, argv); // Parse the command line arguments
     // config_path is the path to the json file containing the application configuration
@@ -32,8 +32,7 @@ int main(int argc, char **argv)
 
     // Open the config file and exit if failed
     std::ifstream file_in(config_path);
-    if (!file_in)
-    {
+    if (!file_in) {
         std::cerr << "Couldn't open file: " << config_path << std::endl;
         return -1;
     }
@@ -48,6 +47,7 @@ int main(int argc, char **argv)
     app.registerState<Menustate>("menu");
     app.registerState<Playstate>("play");
     app.registerState<GameOverstate>("game-over");
+    app.registerState<WinningState>("winning");
     app.registerState<ShaderTestState>("shader-test");
     app.registerState<MeshTestState>("mesh-test");
     app.registerState<TransformTestState>("transform-test");
@@ -58,8 +58,7 @@ int main(int argc, char **argv)
     app.registerState<EntityTestState>("entity-test");
     app.registerState<RendererTestState>("renderer-test");
     // Then choose the state to run based on the option "start-scene" in the config
-    if (app_config.contains(std::string{"start-scene"}))
-    {
+    if (app_config.contains(std::string{"start-scene"})) {
         app.changeState(app_config["start-scene"].get<std::string>());
     }
 
