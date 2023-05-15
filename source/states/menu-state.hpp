d6457e8b7c6c584e262d1258fc3ee4be08036b09
Ahmedsabry11 <ahmedsabry232345@gmail.com>
2023-05-16 00:11:24 +0300

    feat: add score in winning state

--- source/states/menu-state.hpp
+++ source/states/menu-state.hpp
@@ -56,7 +56,8 @@ class Menustate: public our::State {
         menuMaterial->shader->attach("assets/shaders/textured.frag", GL_FRAGMENT_SHADER);
         menuMaterial->shader->link();
         // Then we load the menu texture
-        menuMaterial->texture = our::texture_utils::loadImage("assets/textures/menu.png");
+        menuMaterial->texture = our::texture_utils::loadImage("assets/textures/win.jpg");
+        // menuMaterial->texture = our::texture_utils::loadImage("assets/textures/menu.png");
         // Initially, the menu material will be black, then it will fade in
         menuMaterial->tint = glm::vec4(0.0f, 0.0f, 0.0f, 0.0f);
 
@@ -177,5 +178,20 @@ class Menustate: public our::State {
         delete menuMaterial;
         delete highlightMaterial->shader;
         delete highlightMaterial;
+    }
+    void onImmediateGui() override
+    {
+        ImGui::Begin("Final Score" , 0 , ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove |ImGuiWindowFlags_NoBackground);
+        // setting window position 
+        ImGui::SetWindowPos(ImVec2(450,500));
+        // setting window size
+        ImGui::SetWindowSize(ImVec2(450,50));
+        ImGui::SetWindowFontScale(4.0f);
+        // ImGui::SetNextWindowBgAlpha(0.0f);
+        // writing text to window 
+        ImGui::TextColored(ImVec4(1.0f,1.0f,1.0f,1.0f) , "Score : %d / 100" , getApp()->countPepsi);
+        ImGui::End();
+
+        
     }
 };
